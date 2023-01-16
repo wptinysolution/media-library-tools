05b9fa578a1c470b8113958b539a0f8d54622559
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2023-01-17 01:13:17 +0600

    Media Edit added

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,58 @@
-import  Table from './Component/Table'
+
+import axios from "axios";
+import React, { useMemo, useState, useEffect } from "react";
+
+import Table from "./Component/Table";
+
 function App() {
+    /*
+      - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+      - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+    */
+
+    const columns = [
+            {
+                Header: "Id",
+                Width: "20px",
+            },
+            {
+                Header: "Image",
+                Width: "50px",
+            },
+            {
+                Header: "Title",
+                Width: "150",
+            },
+
+            {
+                Header: "Alt",
+                Width: "150",
+            },
+            {
+                Header: "Caption",
+                Width: "150",
+            },
+            {
+                Header: "Description",
+                Width: "150",
+            },
+        ];
+
+    // data state to store the TV Maze API data. Its initial value is an empty array
+    const [data, setData] = useState([]);
+
+    // Using useEffect to call the API once mounted and set the data
+    useEffect(() => {
+        (async () => {
+            const result = await axios("http://mediaedit.local/wp-json/wp/v2/media");
+            setData(result.data);
+        })();
+    }, []);
+
     return (
-        <div>
-           <Table/>
+        <div className="App">
+            <Table columns={columns} data={data} />
         </div>
     );
 }
-
-export default App;
+export default App
--- src/js/Component/Table.js
+++ src/js/Component/Table.js
@@ -1,25 +1,36 @@
-function Table() {
-	return (
-        <div>
-            <table className="wp-list-table widefat fixed striped">
-                <thead>
-                <tr>
-                    <th> Name </th>
-                </tr>
-                </thead>
-                    <tbody id="the-comment-list">
-                        <td>
-                            Mamun
-                        </td>
-                    </tbody>
-                <tfoot>
-                <tr>
-                    <th> Name </th>
-                </tr>
-                </tfoot>
-            </table>
-        </div>
+// Table.js
+
+import React from "react";
+import { useTable } from "react-table";
+
+export default function Table({ columns, data }) {
+    // Use the useTable Hook to send the columns and data to build the table
+
+    /*
+      Render the UI for your table
+      - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
+    */
+    return (
+        <table className={`wp-list-table widefat fixed striped table-view-list media`}>
+            <thead>
+                 <tr>
+                    {columns.map( ( column, i ) => (
+                        <th width={ column.Width } className={`manage-column`} key={i}>{ column.Header }</th>
+                    ))}
+                 </tr>
+            </thead>
+            <tbody id="the-list">
+                { data.map( ( item, i ) => (
+                    <tr key={i} >
+                        <td width={`50px`}>  { item.id } </td>
+                        <td> <img width={`50`} src={item.source_url}/></td>
+                        <td> { item.title.rendered } </td>
+                        <td> { item.alt_text } </td>
+                        <td> { item.caption.rendered } </td>
+                        <td> { item.description.rendered } </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
-
-export default Table;
