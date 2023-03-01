5fe120830737086ff9d22c7c35544a6d056576c7
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2023-03-01 22:59:53 +0600

    Menu added

--- src/js/Component/App.js
+++ src/js/Component/App.js
@@ -1,16 +1,46 @@
 
 import React from "react";
 
-import { Pagination, Table, Input, Modal, Checkbox, Select, Layout, Button, Space } from 'antd';
+import { Pagination, Table, Input, Modal, Checkbox, Select, Layout, Menu, Button, Space } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 import DataTable from "./Datatable";
-
+const headerStyle = {
+    height: 64,
+    paddingInline: 0,
+    lineHeight: '64px',
+    backgroundColor: '#fff',
+};
 function App() {
     return (
         <div className="tttme-App">
-             <DataTable />
+            <Layout className="layout">
+                <Header className="header" style={headerStyle}>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['listTbale']}
+                        items={[
+                            {
+                                key: 'listTbale',
+                                label: 'List Tbale',
+                            },
+
+                            {
+                                key: 'settings',
+                                label: 'Settings',
+                            },
+                        ]}
+                        onSelect={ ({ item, key, keyPath, selectedKeys, domEvent }) => {
+                            console.log( key );
+                        } }
+                    />
+                </Header>
+                <Content>
+                    <DataTable />
+                </Content>
+            </Layout>
         </div>
     );
 }
