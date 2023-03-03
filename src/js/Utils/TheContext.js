74b9c817ab6df38e17adf5806d6e47ac244b8e40
mamunur105 <rmamunur105@gmail.com>
2023-03-03 11:31:17 +0600

    Fix

--- src/js/Utils/TheContext.js
+++ src/js/Utils/TheContext.js
@@ -3,3 +3,4 @@ import React, { createContext } from 'react';
 // Create a context object with an initial value
 export const TheAppContext = createContext(null);
 export const TheContext = createContext(null);
+export const TheMediaTableContext = createContext(null);
