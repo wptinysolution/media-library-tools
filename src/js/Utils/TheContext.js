c1dc5a9da1e60ee9ea65826653232f928b3673a4
mamunur105 <rmamunur105@gmail.com>
2023-03-03 11:15:40 +0600

    add

--- src/js/Utils/TheContext.js
+++ src/js/Utils/TheContext.js
@@ -1,4 +1,5 @@
 import React, { createContext } from 'react';
 
 // Create a context object with an initial value
+export const TheAppContext = createContext(null);
 export const TheContext = createContext(null);
