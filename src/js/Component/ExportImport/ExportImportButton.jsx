0ecc1d3de0fd5dadcf81e35d3c43a4cd076787be
Mamunur Rashid <rmamunur105@gmail.com>
2024-03-02 19:31:51 +0600

    Remove Un use code

--- src/js/Component/ExportImport/ExportImportButton.jsx
+++ src/js/Component/ExportImport/ExportImportButton.jsx
@@ -18,7 +18,6 @@ import * as Types from "../../Utils/actionType";
 import DownloadCSV from "./DownloadCSV";
 import {mediaCount} from "../../Utils/Data";
 import UploadCsv from "./UploadCsv";
-import ResumeButton from "./ResumeButton";
 
 const buttonStyle = {
     width: '200px',
@@ -34,9 +33,7 @@ function ExportImportButton() {
 
     const [stateValue, dispatch] = useStateValue();
 
-    const [ importRemaining, setImportRemaining] = useState([] );
-
-    const isExportImport = stateValue.exportImport.isExport || stateValue.exportImport.isImport;
+    const isExportImport = stateValue.exportImport.isImport;
 
     const handleExportImport = async ( type ) => {
 
@@ -51,35 +48,17 @@ function ExportImportButton() {
             return;
         }
 
-        const isExport = 'export' === type && 'import' !== type;
-        const isImport = 'import' === type && 'export' !== type;
+        const isImport = 'import' === type;
 
         let exportImport = {
             ...stateValue.exportImport,
-            isExport,
             isImport,
-        }
-
-        if( isExport ){
-            const theMediaCount = await mediaCount();
-            exportImport = {
-                ...exportImport,
-                ...theMediaCount,
-                runImporter: false,
-                runExporter: true
-            }
-        }
-
-        if( isImport ){
-            exportImport = {
-                ...exportImport,
-                runImporter: true,
-                runExporter: false,
-                mediaFiles: [],
-                fileCount : 0,
-                percent : 0,
-                totalPage: 0
-            }
+            runImporter: false,
+            runExporter: false,
+            mediaFiles: [],
+            fileCount : 0,
+            percent : 0,
+            totalPage: 0
         }
 
         await dispatch({
@@ -87,61 +66,6 @@ function ExportImportButton() {
             exportImport: exportImport,
         });
     }
-
-    const confirm = (e) => {
-        handleExportImport( 'export' );
-    };
-    const cancel = (e) => {
-        //console.log(e);
-    };
-
-    const isRemainingImport = () => {
-        const import_remaining = localStorage.getItem( "mlt_import_remaining_history");
-        const remaining = import_remaining ? JSON.parse( import_remaining ) : {};
-        if(  100 > remaining.countPercent  ) {
-           // console.log(remaining)
-            // dispatch({
-            //     type: Types.EXPORT_IMPORT,
-            //     exportImport: {
-            //         ...stateValue.exportImport,
-            //         isExport: true,
-            //         isImport: false,
-            //         runImporter: false,
-            //         totalPage: remaining?.totalPage ? remaining.totalPage : 0,
-            //         mediaFiles: remaining?.exportedMediaFiles ? remaining.exportedMediaFiles : {},
-            //         percent: remaining?.countPercent ? remaining.countPercent : 0
-            //     }
-            // });
-        }
-    }
-
-    const isRemainingExport = () => {
-        const export_remaining = localStorage.getItem( "mlt_exported_history");
-        const remaining = export_remaining ? JSON.parse( export_remaining ) : {};
-        if( remaining?.countPercent && 100 > remaining?.countPercent ) {
-            dispatch({
-                type: Types.EXPORT_IMPORT,
-                exportImport: {
-                    ...stateValue.exportImport,
-                    isExport: true,
-                    isImport: false,
-                    runExporter: false,
-                   // mediaFiles: remaining?.exportedMediaFiles ? remaining.exportedMediaFiles : [],
-                    // pagesRemaining: remaining?.pagesRemaining ? remaining.pagesRemaining : 0,
-                    // totalPage: remaining?.totalPage ? remaining.totalPage : 0,
-                    percent: remaining?.countPercent ? remaining.countPercent : 0
-                }
-            });
-            //console.log( 'Resume' , remaining )
-        }
-    }
-
-    useEffect(() => {
-        isRemainingImport();
-        isRemainingExport();
-    }, []);
-
-    //console.log( 'isExportImport : ', isExportImport );
 
     return (
         <Layout className="layout">
