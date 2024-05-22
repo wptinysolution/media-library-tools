6069991c6da4bba32ec9c1fe21b0738af806faa9
Mamunur Rashid <rmamunur105@gmail.com>
2024-05-23 01:53:02 +0600

    Version:           1.3.4.1

--- media-library-tools.php
+++ media-library-tools.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Media Library Tools
  * Plugin URI:        https://www.wptinysolutions.com/tiny-products/media-library-tools/
  * Description:       Bulk Rename media file, Bulk Edit Title, ALT tags, captions, and descriptions of your media files can improve the organization and SEO score.
- * Version:           1.3.4
+ * Version:           1.3.4.1
  * Author:            Tiny Solutions
  * Author URI:        https://www.wptinysolutions.com/
  * Text Domain:       tsmlt-media-tools
