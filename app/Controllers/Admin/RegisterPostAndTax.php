29be7a2ac9e04352d9cf05299ac34fe93ce76667
Mamunur Rashid <rmamunur105@gmail.com>
2024-07-04 23:32:09 +0600

    Update Register Taxonomies

--- app/Controllers/Admin/RegisterPostAndTax.php
+++ app/Controllers/Admin/RegisterPostAndTax.php
@@ -10,36 +10,41 @@ use TinySolutions\mlt\Traits\SingletonTrait;
  * Register Post and Taxonomy class
  */
 class RegisterPostAndTax {
-    /**
-     * Singleton
-     */
-    use SingletonTrait;
+	/**
+	 * Singleton
+	 */
+	use SingletonTrait;
 
-    /**
-     * Autoload method
-     * @return void
-     */
-    private function __construct() {
-        add_action('init', [ $this , 'register_taxonomies'] );
-    }
+	/**
+	 * Autoload method
+	 *
+	 * @return void
+	 */
+	private function __construct() {
+		add_action( 'init', [ $this , 'register_taxonomies' ] );
+	}
 
-    public function register_taxonomies() {
-        if ( ! is_blog_installed() || ! post_type_exists( 'attachment' ) ) {
-            return;
-        }
-        do_action('tsmlt_register_taxonomy');
+	/**
+	 * Register Taxonomies
+	 *
+	 * @return void
+	 */
+	public function register_taxonomies() {
+		if ( ! is_blog_installed() || ! post_type_exists( 'attachment' ) ) {
+			return;
+		}
+		do_action( 'tsmlt_register_taxonomy' );
 
-        $args = array(
-            'label'        => esc_html__( 'Category', 'tsmlt-media-tools' ),
-            'public'       => true,
-            'rewrite'      => array( 'slug' => 'tsmlt-category' ),
-            'update_count_callback' => '_update_generic_term_count',
-            'hierarchical' => true
-        );
+		$args = [
+			'label'                 => esc_html__( 'Category', 'tsmlt-media-tools' ),
+			'public'                => true,
+			'rewrite'               => [ 'slug' => 'tsmlt-category' ],
+			'update_count_callback' => '_update_generic_term_count',
+			'hierarchical'          => true,
+		];
 
-        register_taxonomy( tsmlt()->category, 'attachment', $args );
+		register_taxonomy( tsmlt()->category, 'attachment', $args );
 
-        do_action('tsmlt_after_register_taxonomy');
-    }
-
+		do_action( 'tsmlt_after_register_taxonomy' );
+	}
 }
