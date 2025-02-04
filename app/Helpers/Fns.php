a72ab2da0caed5f5c751e57b917640a980fc3088
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2025-02-04 10:33:08 +0600

    Search

--- app/Helpers/Fns.php
+++ app/Helpers/Fns.php
@@ -193,26 +193,28 @@ class Fns {
 	 * @param string $orig_image_url
 	 * @return array List of post IDs where the URL is found.
 	 */
-	private static function search_elementor_metadata( $orig_image_url ) {
-		if ( ! defined( 'ELEMENTOR_VERSION' ) ) {
-			return [];
-		}
-		global $wpdb;
-		$table_meta = $wpdb->postmeta;
+    private static function search_elementor_metadata( $orig_image_url ) {
+        if ( ! defined( 'ELEMENTOR_VERSION' ) ) {
+            return [];
+        }
+        global $wpdb;
+        $table_meta               = $wpdb->postmeta;
+        $table_posts              = $wpdb->posts;
+        $useless_types_conditions = self::$useless_types_conditions;
+        $orig_image_url           = esc_sql( $orig_image_url );
+        $orig_image_url           = str_replace( '/', '\/', $orig_image_url );
+        $searchValue              = '%' . str_replace( '\/', '\\\/', $orig_image_url ) . '%';
 
-		$orig_image_url = esc_sql( $orig_image_url );
-		$orig_image_url = str_replace( '/', '\/', $orig_image_url );
-		$searchValue    = '%' . str_replace( '\/', '\\\/', $orig_image_url ) . '%';
-
-		$query = $wpdb->prepare(
-			"SELECT post_id FROM {$table_meta}
-        WHERE meta_key = '_elementor_data'
-        AND meta_value LIKE %s",
-			$searchValue
-		);
-
-		return $wpdb->get_col( $query );
-	}
+        $query = $wpdb->prepare(
+            "SELECT m.post_id FROM {$table_meta} AS m
+		JOIN {$table_posts} AS p ON m.post_id = p.ID
+		WHERE m.meta_key = '_elementor_data'
+		AND m.meta_value LIKE %s
+		AND {$useless_types_conditions}",
+            $searchValue
+        );
+        return $wpdb->get_col( $query );
+    }
 	/**
 	 * @param $orig_image_url
 	 * @param $new_image_url
