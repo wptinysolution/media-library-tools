226377385ada1d5e35a53a6bec0543eadf183ec4
Mamunur Rashid <rmamunur105@gmail.com>
2025-05-07 00:11:32 +0600

    .

--- src/js/Component/ExportImport/ExportButton.jsx
+++ src/js/Component/ExportImport/ExportButton.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 
-import { Layout, Button, Spin, Space, Typography} from 'antd';
+import {Layout, Button, Spin, Space, Typography, Progress} from 'antd';
 
 const { Title, Text } = Typography;
 
@@ -33,6 +33,8 @@ const buttonStyle = {
 function ExportButton() {
 
     const [stateValue, dispatch] = useStateValue();
+
+    const [percent, setPercent] = useState(0);
 
     const isExport = stateValue.exportImport.isExport;
 
@@ -73,35 +75,44 @@ function ExportButton() {
     return (
         <>
             <MainHeader/>
-            <Layout className="layout">
-                <Content style={{
-                    padding: '100px',
-                    borderRadius: '5px',
-                    boxShadow: 'rgb(0 0 0 / 1%) 0px 0 20px',
+            <Layout className="layout" style={{
+                padding: '100px',
+                borderRadius: '5px',
+                boxShadow: 'rgb(0 0 0 / 1%) 0px 0 20px',
+            }} >
+
+                <Content style={ {
                     display: 'flex',
-                    alignItems: 'center'
-                }}>
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                    gap: '15px'
+                } } >
+                    {
+                        isExport ? <Progress
+                            className={`progressbar-height`}
+                            style={{
+                                height: '30px',
+                                marginTop: 'auto'
+                            }}
+                            showInfo={true} percent={percent}
+                        /> : null
+                    }
                     <Layout
                         style={ {
-                            padding: '50px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: '15px'
                         } }
                     >
-                            <Content className={`csv-export-import-btn-wrapper`}
-                                     style={ {
-                                         display: 'flex',
-                                         justifyContent: 'center',
-                                         gap: '15px'
-                                     } }
-                            >
-                                <Button
-                                    type="primary"
-                                    size={`large`}
-                                    style={ buttonStyle }
-                                    onClick={ () => handleExport( 'export' ) }
-                                >
-                                    <ImportOutlined/> Run Exporter
-                                </Button>
-                            </Content>
+                        <Button
+                            type="primary"
+                            size={`large`}
+                            style={ buttonStyle }
+                            onClick={ () => handleExport( 'export' ) }
+                        >
+                            <ImportOutlined/> Run Exporter
+                        </Button>
+
                     </Layout>
                 </Content>
             </Layout>
