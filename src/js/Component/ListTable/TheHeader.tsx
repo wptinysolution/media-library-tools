e4b8a5ea36fa4a799f1a41be5b9dd317a67da4ac
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2026-02-27 12:02:57 +0600

    .

--- src/js/Component/ListTable/TheHeader.tsx
+++ src/js/Component/ListTable/TheHeader.tsx
@@ -10,7 +10,7 @@ const bulkOptions = [
     { value: 'csv_export', label: 'Export CSV' },
     { value: 'trash', label: 'Move to Trash' },
     { value: 'inherit', label: 'Restore' },
-    { value: 'update', label: 'Update' },
+    // { value: 'update', label: 'Update' },
     { value: 'delete', label: 'Delete Permanently' },
     { value: 'searchUses', label: 'Search Uses' },
     { value: 'bulkedit', label: 'Bulk Edit' },
